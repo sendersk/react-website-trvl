bbeee7bb9bc702583b4b681611605a8ec9a8dd79
Przemyslaw Senderski <psenderski94@gmail.com>
2021-01-04 23:31:56 +0100

    change state onClick function created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,10 @@ import React, {useState} from 'react'
 import { Link } from "react-router-dom"
 
 function Navbar() {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click)
+
     return (
         <>
           <nav className="navbar">
@@ -9,6 +13,9 @@ function Navbar() {
               <Link to="/" className="navbar-logo">
                 TRVL <i className="fab fa-typo3" />
               </Link>
+              <div className="menu-icon" onClick={handleClick}>
+                <i className={click ? "fas fa-times" : "fas fa-bars"} />
+              </div>
             </div>
           </nav>
         </>
