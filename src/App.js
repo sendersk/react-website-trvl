59c078fd924b0f7e8f1630702e659b0f12c23958
Przemyslaw Senderski <psenderski94@gmail.com>
2021-01-04 23:23:26 +0100

    content modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from "react";
-import "./App.css";
+import React from "react"
+import Navbar from "./components/Navbar"
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import "./App.css"
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+     <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact />
+      </Switch>
+     </Router>
+    </>
   );
 }
 
