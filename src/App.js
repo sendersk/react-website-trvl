e6159bf5f69654484c4c781f9cd2613fab500a35
Przemyslaw Senderski <psenderski94@gmail.com>
2021-01-05 20:35:40 +0100

    App.js code refactored

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from "react"
-import Navbar from "./components/Navbar"
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
-import "./App.css"
+import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <>
-     <Router>
-      <Navbar />
-      <Switch>
-        <Route path="/" exact />
-      </Switch>
-     </Router>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact />
+        </Switch>
+      </Router>
     </>
   );
 }
