17adef2458f85e04901a71f50c711896d7d03965
Przemyslaw Senderski <psenderski94@gmail.com>
2021-01-06 20:50:32 +0100

    new CardItem added

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,6 +2,9 @@ import React from "react";
 import CardItem from "./CardItem";
 import "./Cards.css";
 import imageNo2 from "../assets/images/img-2.jpg";
+import imageNo3 from "../assets/images/img-3.jpg";
+import imageNo4 from "../assets/images/img-4.jpg";
+import imageNo8 from "../assets/images/img-8.jpg";
 import imageNo9 from "../assets/images/img-9.jpg";
 
 function Cards() {
@@ -19,6 +22,26 @@ function Cards() {
             />
             <CardItem
               src={imageNo2}
+              text="Travel through the Islands of Bali in a Private Cruise"
+              label="Luxury"
+              path="/services"
+            />
+          </ul>
+          <ul className="cards__item">
+            <CardItem
+              src={imageNo3}
+              text="Explore the hidden waterfall deep inside the Amazon Jungle"
+              label="Adventure"
+              path="/services"
+            />
+            <CardItem
+              src={imageNo4}
+              text="Travel through the Islands of Bali in a Private Cruise"
+              label="Luxury"
+              path="/services"
+            />
+            <CardItem
+              src={imageNo8}
               text="Travel through the Islands of Bali in a Private Cruise"
               label="Luxury"
               path="/services"
