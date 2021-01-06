271558cca6380c39b7ad7cd07624f2d511b7e4b4
Przemyslaw Senderski <psenderski94@gmail.com>
2021-01-06 14:08:10 +0100

    src to video changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,12 +1,12 @@
 import React from "react";
-import "../App.css"
+import "../App.css";
 import { Button } from "./Button";
 import "./HeroSection.css";
 
 function HeroSection() {
   return (
     <div className="hero-container">
-      <video src="/public/videos/video-2.mp4" autoPlay loop muted />
+      <video src="./assets/videos/video-1.mp4" autoPlay loop muted />
       <h1>ADVENTURES AWAITS</h1>
       <p>What are you waiting for?</p>
       <div className="hero-btns">
