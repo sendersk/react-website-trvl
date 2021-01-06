1962a793c993eb57027c8587ba6fa770542e80c6
Przemyslaw Senderski <psenderski94@gmail.com>
2021-01-06 19:26:19 +0100

    path to video-2 changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,11 +2,12 @@ import React from "react";
 import "../App.css";
 import { Button } from "./Button";
 import "./HeroSection.css";
+import myVideo from "../assets/videos/video-2.mp4" 
 
 function HeroSection() {
   return (
     <div className="hero-container">
-      <video src="./assets/videos/video-1.mp4" autoPlay loop muted />
+      <video src={myVideo} autoPlay loop muted />
       <h1>ADVENTURES AWAITS</h1>
       <p>What are you waiting for?</p>
       <div className="hero-btns">
