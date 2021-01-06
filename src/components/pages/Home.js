88631962e50e825ee4e769c296d33b2723a80f7e
Przemyslaw Senderski <psenderski94@gmail.com>
2021-01-06 20:15:46 +0100

    Cards component added

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../../App.css";
-import HeroSection from "./../HeroSection";
+import Cards from "../Cards";
+import HeroSection from "../HeroSection";
 
 function Home() {
   return (
     <>
-      <HeroSection></HeroSection>
+      <HeroSection />
+      <Cards />
     </>
   );
 }
