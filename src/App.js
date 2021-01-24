59a60ab8634f3c12ed521682618c62b8fab7bce5
Przemyslaw Senderski <psenderski94@gmail.com>
2021-01-24 17:47:40 +0100

    route to Products page added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/pages/Home";
 import Services from "./components/pages/Services"
+import Products from "./components/pages/Products"
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/services" component={Services} />
+          <Route path="/products" component={Products} />
         </Switch>
       </Router>
     </>
