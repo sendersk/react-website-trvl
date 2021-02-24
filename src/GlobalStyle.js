2eba94747750f35f37e375ea09778741466f9b60
Przemyslaw Senderski <psenderski94@gmail.com>
2021-02-24 18:21:46 +0100

    GlobalStyle component created

---
+++ src/GlobalStyle.js
@@ -0,0 +1,17 @@
+import {createGlobalStyle} from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+    }
+
+    *, ::after, ::before {
+        box-sizing: inherit;
+    } 
+
+    body {
+        font-family: "PT Sans", sans-serif;
+        margin: 0;
+        padding: 0;
+    }
+`
