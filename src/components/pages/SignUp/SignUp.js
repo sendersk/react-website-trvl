02ce9c3e5f135b58f59fb59e65f816c73b3370a4
Przemyslaw Senderski <psenderski94@gmail.com>
2021-02-24 19:27:42 +0100

    SignUp component created

---
+++ src/components/pages/SignUp/SignUp.js
@@ -0,0 +1,6 @@
+import React from "react";
+import {Wrapper} from "./styled"
+
+export default function SignUp() {
+  return <Wrapper>SIGN UP</Wrapper>;
+}
---
+++ src/components/pages/SignUp/styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import background from "../../../assets/images/img-8.jpg";
+
+export const Wrapper = styled.div`
+  display: flex;
+  height: 90vh;
+  align-items: center;
+  justify-content: center;
+  font-size: 3rem;
+  background-image: url("${background}");
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  color: #fff;
+  font-size: 100px;
+`;
